578a8bf5affbb469fad94dbef31f971f92a3b495
Marko <mark.maljuk@gmail.com>
2019-04-15 16:55:11 +0300

    [/app/components/]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ContactPageComponent } from './components/contact-page/contact-page.component';
+import { InfoPageComponent } from './components/info-page/info-page.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
+
+const routs = [
+  {
+    path: '', component: MainPageComponent
+  },
+  {
+    path: 'contact', component: ContactPageComponent
+  },
+  {
+    path: 'info', component: InfoPageComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContactPageComponent,
+    InfoPageComponent,
+    MainPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routs)
+  ],
+  exports: [
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
