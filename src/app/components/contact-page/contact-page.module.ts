64babccccc01f59b1bd903c25781510073a6bec5
Svitlana Melashenko <svetapogranichna@gmail.com>
2019-05-01 11:33:34 +0300

    preview contact page design

---
+++ src/app/components/contact-page/contact-page.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+
+import { ContactPageComponent } from './contact-page.component';
+
+@NgModule({
+    imports: [],
+    exports: [
+      ContactPageComponent,
+    ],
+    declarations: [
+      ContactPageComponent,
+    ]
+  })
+  export class ContactPageModule{ }
