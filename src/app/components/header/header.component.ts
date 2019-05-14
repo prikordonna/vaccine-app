cec8ca6c8b49b542ecf5bfcd9d0e94f8b6bc5d64
marcelo3313 <mark.maljuk@gmail.com>
2019-05-14 11:51:32 +0300

    deployment

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -9,7 +9,7 @@ import { AuthService } from '../../services/auth.service';
 export class HeaderComponent implements OnInit {
 
   constructor(
-    private auth: AuthService,
+    public auth: AuthService,
   ) {}
 
   ngOnInit() {
